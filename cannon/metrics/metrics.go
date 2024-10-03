c736f18d4132158380acd9dbc413878a26b50a81
mbaxter <meredith@oplabs.co>
2024-10-03 15:14:23 -0400

    cannon: Track counter metric variables

--- cannon/metrics/metrics.go
+++ cannon/metrics/metrics.go
@@ -9,35 +9,47 @@ type Metrics interface {
 }
 
 type baseMetricsImpl struct {
-	lastLLOpStep uint64
+	lastLLOpStep                    uint64
+	rmwSuccessCount                 uint64
+	rmwFailureCount                 uint64
+	rmwReservationInvalidationCount uint64
+	rmwReservationOverwriteCount    uint64
 }
 
 var _ Metrics = (*baseMetricsImpl)(nil)
 
 func NewNoopMetrics() Metrics {
 	return &baseMetricsImpl{
-		lastLLOpStep: 0,
+		lastLLOpStep:                    0,
+		rmwSuccessCount:                 0,
+		rmwFailureCount:                 0,
+		rmwReservationInvalidationCount: 0,
+		rmwReservationOverwriteCount:    0,
 	}
 }
 
 func (m *baseMetricsImpl) TrackLLOp(step uint64, overwritesExistingReservation bool) {
 	if overwritesExistingReservation {
-		m.recordRMWOverwritten()
+		m.rmwReservationOverwriteCount += 1
+		m.recordRMWOverwritten(m.rmwReservationOverwriteCount)
 	}
 	m.lastLLOpStep = step
 }
 
 func (m *baseMetricsImpl) TrackSCOpSuccess(step uint64) {
+	m.rmwSuccessCount += 1
 	totalSteps := step - m.lastLLOpStep
-	m.recordRMWSuccess(totalSteps)
+	m.recordRMWSuccess(m.rmwSuccessCount, totalSteps)
 }
 
 func (m *baseMetricsImpl) TrackSCOpFailure() {
-	m.recordRMWFailure()
+	m.rmwFailureCount += 1
+	m.recordRMWFailure(m.rmwFailureCount)
 }
 
 func (m *baseMetricsImpl) TrackLLReservationInvalidated() {
-	m.recordRMWInvalidated()
+	m.rmwReservationInvalidationCount += 1
+	m.recordRMWInvalidated(m.rmwReservationInvalidationCount)
 }
 
 func (m *baseMetricsImpl) TrackPreemption(stepsSinceLastPreemption uint64) {
@@ -46,12 +58,12 @@ func (m *baseMetricsImpl) TrackPreemption(stepsSinceLastPreemption uint64) {
 
 // TODO(#12061) Override or implement the following for the production metrics implementation
 
-func (n *baseMetricsImpl) recordRMWSuccess(totalSteps uint64) {}
+func (n *baseMetricsImpl) recordRMWSuccess(count uint64, totalSteps uint64) {}
 
-func (n *baseMetricsImpl) recordRMWFailure() {}
+func (n *baseMetricsImpl) recordRMWFailure(count uint64) {}
 
-func (n *baseMetricsImpl) recordRMWInvalidated() {}
+func (n *baseMetricsImpl) recordRMWInvalidated(count uint64) {}
 
-func (n *baseMetricsImpl) recordRMWOverwritten() {}
+func (n *baseMetricsImpl) recordRMWOverwritten(count uint64) {}
 
 func (n *baseMetricsImpl) recordPreemption(stepsSinceLastPreemption uint64) {}
