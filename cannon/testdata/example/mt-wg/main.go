6b272bd14f0ddad564a28c138a6e2a72d736b3e6
mbaxter <meredith@oplabs.co>
2024-11-13 17:00:32 -0500

    cannon: Port waitgroup_test.go

--- cannon/testdata/example/mt-wg/main.go
+++ cannon/testdata/example/mt-wg/main.go
@@ -3,44 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
-	"runtime"
-	"sync"
-	"sync/atomic"
+	"testing"
 )
 
 func main() {
-	// try some concurrency!
-	var wg sync.WaitGroup
-	wg.Add(2)
-	var x atomic.Int32
-	go func() {
-		x.Add(2)
-		wg.Done()
-	}()
-	go func() {
-		x.Add(40)
-		wg.Done()
-	}()
-	wg.Wait()
-	fmt.Printf("waitgroup result: %d\n", x.Load())
+	runTest(TestWaitGroup, "TestWaitGroup")
+	runTest(TestWaitGroupMisuse, "TestWaitGroupMisuse")
+	runTest(TestWaitGroupRace, "TestWaitGroupRace")
+	runTest(TestWaitGroupAlign, "TestWaitGroupAlign")
 
-	// channels
-	a := make(chan int, 1)
-	b := make(chan int)
-	c := make(chan int)
-	go func() {
-		t0 := <-a
-		b <- t0
-	}()
-	go func() {
-		t1 := <-b
-		c <- t1
-	}()
-	a <- 1234
-	out := <-c
-	fmt.Printf("channels result: %d\n", out)
+	fmt.Println("WaitGroup tests passed")
+}
 
-	// try a GC! (the runtime might not have run one yet)
-	runtime.GC()
-	_, _ = os.Stdout.Write([]byte("GC complete!\n"))
+func runTest(testFunc func(*testing.T), name string) {
+	t := &testing.T{}
+	testFunc(t)
+	if t.Failed() {
+		fmt.Printf("Test failed: %v\n", name)
+		os.Exit(1)
+	} else {
+		fmt.Printf("Test passed: %v\n", name)
+	}
 }
